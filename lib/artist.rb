a303fab07db04a0f8c24697edded7bd60411302b
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-12 19:03:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,31 @@
 class Artist
+   attr_accessor :name
 
-  attr_accessor :name, :songs
+   @@all = []
 
-  @@song_count = 0
+   def initialize(name)
+     @name = name
+     @@all << self
+   end
 
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
+   def self.all
+     @@all
+   end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
+   def add_song(song)
+     song.artist = self
+   end
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
-  end
+   def add_song_by_name(name)
+     song = Song.new(name)
+     song.artist = self
+   end
 
-  def self.song_count
-    @@song_count
-  end
+   def songs
+     Song.all.select {|song| song.artist == self}
+   end
 
-end
+   def self.song_count
+     Song.all.count
+   end
+ end
