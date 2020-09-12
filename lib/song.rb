bd4ba93cfa100a938f767863728f3903cbd1ba6d
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-12 19:02:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,16 @@
 class Song
-  attr_accessor :name, :artist
+   attr_accessor :artist, :name
 
-  def initialize(name)
-    @name = name
-  end
+   def initialize(name)
+     @name = name
+   end
 
-  def artist_name
-    if @artist == nil
-      return nil
-    else
-      @artist.name
-    end
-  end
-end
+   def artist_name
+     if artist
+       self.artist.name
+     else
+       nil
+     end
+   end
+
+ end
