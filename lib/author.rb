4430eba53b29b9f2c6a6eed9e7d92a79b767e2c2
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-12 19:03:46 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,28 +1,31 @@
 class Author
+   attr_accessor :name
 
-  attr_accessor :name, :posts
+   @@all = []
 
-  @@post_count = 0
-  
-  def initialize(name)
-    @name = name
-    @posts = []
-  end
+   def initialize(name)
+     @name = name
+     @@all << self
+   end
 
-  def add_post(post)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
+   def self.all
+     @@all
+   end
 
-  def add_post_by_title(title)
-    post = Post.new(title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
+   def add_post(post)
+     post.author = self
+   end
 
-  def self.post_count
-    @@post_count
-  end
-end
+   def add_post_by_title(title)
+     post = Post.new(title)
+     post.author = self
+   end
+
+   def posts
+     Post.all.select {|post| post.author == self}
+   end
+
+   def self.post_count
+     Post.all.count
+   end
+ end
